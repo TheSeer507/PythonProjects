36c1831d9bfff5c1dbb1a9c01d3082d4b5c2fc56
Patel Hasan <Hasan.Patel@tetrapak.com>
2022-11-04 14:04:12 -0500

    Completed 1st Python project - demo

--- Madlibs/madlibs.py
+++ Madlibs/madlibs.py
@@ -1,2 +1,9 @@
-# string concatenation
+adj = input("Adjective: ")
+verb = input("Verb 1: ")
+verb2 = input("Verb 2: ")
+famous_person = input("Famous Person: ")
 
+madlib = f"Computer programming is so {adj}! It makes me excited all the time because \
+I love to {verb}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
